94c07436b34f4110abf1ebf42b38c317f88a3bd7
Cong Thanh <pdcthanh112.dev@gmail.com>
2022-11-30 20:35:50 +0700

    30/11/2022

--- src/views/Recuirment/RecruitmentRequest/ExpiredRecruitmentRequest/ExpiredRecruitmentRequest.jsx
+++ src/views/Recuirment/RecruitmentRequest/ExpiredRecruitmentRequest/ExpiredRecruitmentRequest.jsx
@@ -17,34 +17,29 @@ import Paper from '@mui/material/Paper';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
 import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
 const ExpiredRecruitmentRequest = () => {
 
   const [listExpiredRecruitmentRequest, setListExpiredRecruitmentRequest] = useState([])
   const [isLoading, setIsLoading] = useState(true)
-  const [pagination, setPagination] = useState({ totalPage: 10, currentPage: 1 })
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true)
-      const response = await getExpiryDateRecruitmentRequest(pagination.currentPage - 1, 10);
+      const response = await getExpiryDateRecruitmentRequest();
       if (response) {
         setListExpiredRecruitmentRequest(response.data.responseList)
-        setPagination({ ...pagination, totalPage: response.data.totalPage })
         setIsLoading(false)
       }
     }
     fetchData();
-  }, [pagination.currentPage])
+  }, [])
 
   const [order, setOrder] = React.useState('asc');
   const [orderBy, setOrderBy] = React.useState('calories');
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
-  const [dense, setDense] = React.useState(false);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
   const handleRequestSort = (event, property) => {
@@ -55,19 +50,19 @@ const ExpiredRecruitmentRequest = () => {
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
-      const newSelected = rows.map((n) => n.name);
+      const newSelected = listExpiredRecruitmentRequest.map((n) => n.id);
       setSelected(newSelected);
       return;
     }
     setSelected([]);
   };
 
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
+  const handleClick = (event, id) => {
+    const selectedIndex = selected.indexOf(id);
     let newSelected = [];
 
     if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
+      newSelected = newSelected.concat(selected, id);
     } else if (selectedIndex === 0) {
       newSelected = newSelected.concat(selected.slice(1));
     } else if (selectedIndex === selected.length - 1) {
@@ -91,53 +86,39 @@ const ExpiredRecruitmentRequest = () => {
     setPage(0);
   };
 
-  const handleChangeDense = (event) => {
-    setDense(event.target.checked);
-  };
+  const isSelected = (id) => selected.indexOf(id) !== -1;
 
-  const isSelected = (name) => selected.indexOf(name) !== -1;
-
-  // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - listExpiredRecruitmentRequest.length) : 0;
 
   return (
     <React.Fragment>
       {isLoading ? <ReactLoading className='mx-auto my-5' type='spinningBubbles' color='#bfbfbf' /> :
-          <Box sx={{ width: '100%' }}>
+        <Box sx={{ width: '100%' }}>
           <Paper sx={{ width: '100%', mb: 2 }}>
-            <EnhancedTableToolbar numSelected={selected.length} />
+            <EnhancedTableToolbar numSelected={selected.length} listSelected={selected}/>
             <TableContainer>
-              <Table
-                sx={{ minWidth: 750 }}
-                aria-labelledby="tableTitle"
-                size={dense ? 'small' : 'medium'}
-              >
+              <Table sx={{ minWidth: 750 }}>
                 <EnhancedTableHead
                   numSelected={selected.length}
-                  order={order}
-                  orderBy={orderBy}
                   onSelectAllClick={handleSelectAllClick}
                   onRequestSort={handleRequestSort}
-                  rowCount={rows.length}
+                  rowCount={listExpiredRecruitmentRequest.length}
                 />
                 <TableBody>
-                  {/* if you don't need to support IE11, you can replace the `stableSort` call with:
-                     rows.sort(getComparator(order, orderBy)).slice() */}
-                  {stableSort(rows, getComparator(order, orderBy))
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map((row, index) => {
-                      const isItemSelected = isSelected(row.name);
-                      const labelId = `enhanced-table-checkbox-${index}`;
     
+                  {listExpiredRecruitmentRequest.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => {
+                      const isItemSelected = isSelected(row.id);
+                      const labelId = `enhanced-table-checkbox-${index}`;
+
                       return (
                         <TableRow
                           hover
-                          onClick={(event) => handleClick(event, row.name)}
+                          onClick={(event) => handleClick(event, row.id)}
                           role="checkbox"
                           aria-checked={isItemSelected}
                           tabIndex={-1}
-                          key={row.name}
+                          key={row.id}
                           selected={isItemSelected}
                         >
                           <TableCell padding="checkbox">
@@ -157,19 +138,15 @@ const ExpiredRecruitmentRequest = () => {
                           >
                             {row.name}
                           </TableCell>
-                          <TableCell align="right">{row.calories}</TableCell>
-                          <TableCell align="right">{row.fat}</TableCell>
-                          <TableCell align="right">{row.carbs}</TableCell>
-                          <TableCell align="right">{row.protein}</TableCell>
+                          <TableCell align="right">{row.creator.name}</TableCell>
+                          <TableCell align="right">{row.planDetail.amount}</TableCell>
+                          <TableCell align="right">{row.planDetail.reason}</TableCell>                         
+                          <TableCell align="right">{row.expiryDate}</TableCell>
                         </TableRow>
                       );
                     })}
                   {emptyRows > 0 && (
-                    <TableRow
-                      style={{
-                        height: (dense ? 33 : 53) * emptyRows,
-                      }}
-                    >
+                    <TableRow>
                       <TableCell colSpan={6} />
                     </TableRow>
                   )}
@@ -179,120 +156,59 @@ const ExpiredRecruitmentRequest = () => {
             <TablePagination
               rowsPerPageOptions={[5, 10, 25]}
               component="div"
-              count={rows.length}
+              count={listExpiredRecruitmentRequest.length}
               rowsPerPage={rowsPerPage}
               page={page}
               onPageChange={handleChangePage}
               onRowsPerPageChange={handleChangeRowsPerPage}
             />
           </Paper>
-          <FormControlLabel
-            control={<Switch checked={dense} onChange={handleChangeDense} />}
-            label="Dense padding"
-          />
+
         </Box>
-        }
+      }
     </React.Fragment>
   )
 }
 
 export default ExpiredRecruitmentRequest
 
-function createData(name, calories, fat, carbs, protein) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-  };
-}
-
-const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
-];
-
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
 const headCells = [
   {
     id: 'name',
     numeric: false,
     disablePadding: true,
-    label: 'Dessert (100g serving)',
+    label: 'Recruitment name',
   },
   {
     id: 'calories',
     numeric: true,
     disablePadding: false,
-    label: 'Calories',
-  },
-  {
-    id: 'fat',
-    numeric: true,
-    disablePadding: false,
-    label: 'Fat (g)',
+    label: 'Creator',
   },
   {
     id: 'carbs',
     numeric: true,
     disablePadding: false,
-    label: 'Carbs (g)',
+    label: 'Amount',
   },
+  {
+    id: 'fat',
+    numeric: true,
+    disablePadding: false,
+    label: 'Reason',
+  },
+
   {
     id: 'protein',
     numeric: true,
     disablePadding: false,
-    label: 'Protein (g)',
+    label: 'Expiry Date',
   },
 ];
 
 function EnhancedTableHead(props) {
-  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
-    props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
+  const { onSelectAllClick, order, orderBy, numSelected, rowCount } = props;
+
 
   return (
     <TableHead>
@@ -315,14 +231,10 @@ function EnhancedTableHead(props) {
             padding={headCell.disablePadding ? 'none' : 'normal'}
             sortDirection={orderBy === headCell.id ? order : false}
           >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
+            <TableSortLabel>
               {headCell.label}
               {orderBy === headCell.id ? (
-                <Box component="span" >
+                <Box component="span">
                   {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
                 </Box>
               ) : null}
@@ -336,19 +248,10 @@ function EnhancedTableHead(props) {
 
 
 function EnhancedTableToolbar(props) {
-  const { numSelected } = props;
+  const { numSelected, listSelected } = props;
 
   return (
-    <Toolbar
-      // sx={{
-      //   pl: { sm: 2 },
-      //   pr: { xs: 1, sm: 1 },
-      //   ...(numSelected > 0 && {
-      //     bgcolor: (theme) =>
-      //       alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-      //   }),
-      // }}
-    >
+    <Toolbar>
       {numSelected > 0 ? (
         <Typography
           sx={{ flex: '1 1 100%' }}
@@ -365,14 +268,14 @@ function EnhancedTableToolbar(props) {
           id="tableTitle"
           component="div"
         >
-          Nutrition
+          Choose
         </Typography>
       )}
 
       {numSelected > 0 ? (
         <Tooltip title="Delete">
           <IconButton>
-            <DeleteIcon />
+            <DeleteIcon onClick={() => {console.log(listSelected);}}/>
           </IconButton>
         </Tooltip>
       ) : (
@@ -387,155 +290,3 @@ function EnhancedTableToolbar(props) {
 }
 
 
-// export default function EnhancedTable() {
-//   const [order, setOrder] = React.useState('asc');
-//   const [orderBy, setOrderBy] = React.useState('calories');
-//   const [selected, setSelected] = React.useState([]);
-//   const [page, setPage] = React.useState(0);
-//   const [dense, setDense] = React.useState(false);
-//   const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-//   const handleRequestSort = (event, property) => {
-//     const isAsc = orderBy === property && order === 'asc';
-//     setOrder(isAsc ? 'desc' : 'asc');
-//     setOrderBy(property);
-//   };
-
-//   const handleSelectAllClick = (event) => {
-//     if (event.target.checked) {
-//       const newSelected = rows.map((n) => n.name);
-//       setSelected(newSelected);
-//       return;
-//     }
-//     setSelected([]);
-//   };
-
-//   const handleClick = (event, name) => {
-//     const selectedIndex = selected.indexOf(name);
-//     let newSelected = [];
-
-//     if (selectedIndex === -1) {
-//       newSelected = newSelected.concat(selected, name);
-//     } else if (selectedIndex === 0) {
-//       newSelected = newSelected.concat(selected.slice(1));
-//     } else if (selectedIndex === selected.length - 1) {
-//       newSelected = newSelected.concat(selected.slice(0, -1));
-//     } else if (selectedIndex > 0) {
-//       newSelected = newSelected.concat(
-//         selected.slice(0, selectedIndex),
-//         selected.slice(selectedIndex + 1),
-//       );
-//     }
-
-//     setSelected(newSelected);
-//   };
-
-//   const handleChangePage = (event, newPage) => {
-//     setPage(newPage);
-//   };
-
-//   const handleChangeRowsPerPage = (event) => {
-//     setRowsPerPage(parseInt(event.target.value, 10));
-//     setPage(0);
-//   };
-
-//   const handleChangeDense = (event) => {
-//     setDense(event.target.checked);
-//   };
-
-//   const isSelected = (name) => selected.indexOf(name) !== -1;
-
-//   // Avoid a layout jump when reaching the last page with empty rows.
-//   const emptyRows =
-//     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-
-//   return (
-//     <Box sx={{ width: '100%' }}>
-//       <Paper sx={{ width: '100%', mb: 2 }}>
-//         <EnhancedTableToolbar numSelected={selected.length} />
-//         <TableContainer>
-//           <Table
-//             sx={{ minWidth: 750 }}
-//             aria-labelledby="tableTitle"
-//             size={dense ? 'small' : 'medium'}
-//           >
-//             <EnhancedTableHead
-//               numSelected={selected.length}
-//               order={order}
-//               orderBy={orderBy}
-//               onSelectAllClick={handleSelectAllClick}
-//               onRequestSort={handleRequestSort}
-//               rowCount={rows.length}
-//             />
-//             <TableBody>
-//               {/* if you don't need to support IE11, you can replace the `stableSort` call with:
-//                  rows.sort(getComparator(order, orderBy)).slice() */}
-//               {stableSort(rows, getComparator(order, orderBy))
-//                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-//                 .map((row, index) => {
-//                   const isItemSelected = isSelected(row.name);
-//                   const labelId = `enhanced-table-checkbox-${index}`;
-
-//                   return (
-//                     <TableRow
-//                       hover
-//                       onClick={(event) => handleClick(event, row.name)}
-//                       role="checkbox"
-//                       aria-checked={isItemSelected}
-//                       tabIndex={-1}
-//                       key={row.name}
-//                       selected={isItemSelected}
-//                     >
-//                       <TableCell padding="checkbox">
-//                         <Checkbox
-//                           color="primary"
-//                           checked={isItemSelected}
-//                           inputProps={{
-//                             'aria-labelledby': labelId,
-//                           }}
-//                         />
-//                       </TableCell>
-//                       <TableCell
-//                         component="th"
-//                         id={labelId}
-//                         scope="row"
-//                         padding="none"
-//                       >
-//                         {row.name}
-//                       </TableCell>
-//                       <TableCell align="right">{row.calories}</TableCell>
-//                       <TableCell align="right">{row.fat}</TableCell>
-//                       <TableCell align="right">{row.carbs}</TableCell>
-//                       <TableCell align="right">{row.protein}</TableCell>
-//                     </TableRow>
-//                   );
-//                 })}
-//               {emptyRows > 0 && (
-//                 <TableRow
-//                   style={{
-//                     height: (dense ? 33 : 53) * emptyRows,
-//                   }}
-//                 >
-//                   <TableCell colSpan={6} />
-//                 </TableRow>
-//               )}
-//             </TableBody>
-//           </Table>
-//         </TableContainer>
-//         <TablePagination
-//           rowsPerPageOptions={[5, 10, 25]}
-//           component="div"
-//           count={rows.length}
-//           rowsPerPage={rowsPerPage}
-//           page={page}
-//           onPageChange={handleChangePage}
-//           onRowsPerPageChange={handleChangeRowsPerPage}
-//         />
-//       </Paper>
-//       <FormControlLabel
-//         control={<Switch checked={dense} onChange={handleChangeDense} />}
-//         label="Dense padding"
-//       />
-//     </Box>
-//   );
-// }
